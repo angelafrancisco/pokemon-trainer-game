9c0704e07243d362f7c46e4086bbd9ee6dd15eaa
Angela Francisco <angelafrancisco@Nathans-MacBook-Pro-2.local>
2022-03-02 00:08:16 -0800

    Updated js, html, css for web browser functionality

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # Choose Your Own Adventure: Pokemon Trainer!
 
-Created February 2022. 
-General Assembly project using terminal for a "Choose your own adventure" game.
+**March 2022. First web browser game for General Assembly.**
 
-I created a "Choose Your Own Adventure" style game in `app.js`.  This is a story where the user is frequently asked what the protagonist should do.  The story progresses based on what the user says should happen.
+Originally created February 2022, as a terminal game in the style of a "choose your own adventure" game. Updated March 2022 as a web browser game.
 
 
-## How to Play:
+## How to Play
 
-You are a new Pokemon Trainer who is aspiring to be the best trainer you can be! Follow the terminal prompts to:
+You are a new Pokemon Trainer who is aspiring to be the best trainer you can be! Follow the game prompts to:
 - Select your first Pokemon
 - Train your Pokemon
 - Feed your Pokemon
@@ -16,35 +15,28 @@ You are a new Pokemon Trainer who is aspiring to be the best trainer you can be!
 - And have fun!
 
 
-## Game Components:
+## To Play on Web Browser
+
+Game hosted on GitHub Pages:
+
+https://angelafrancisco.github.io/pokemon-trainer-game/
+
+
+## Game Requirements
 
 The game is structured in sections, each containing two parts: what just happend and a prompt for what to do next.
 
-- Used the `prompt()` function to retrieve input from the user.
+Terminal game version:
+- Used the `prompt()` function to retrieve input from user.
 - Used functions to take parameters relating to what the user has chosen to do.
 - Used variables to keep track of things that have happened in the story (e.g. accumulated health points, tasks, etc.)
 - Ended game with `process.exit()`
 
-## To Play on Web Browser:
-
-Game hosted on GitHub Pages:
-
-https://angelafrancisco.github.io/choose-your-own-adventure/
-
-## To Play Locally:
-
-Clone this repository from GitHub:
-```
-$ git clone https://github.com/angelafrancisco/choose-your-own-adventure.git
-```
-Open project directory. From directory, install NPM packages:
-```
-$ npm i
-```
-This will install the `prompt-sync` package which will allow you to run the `prompt()` function.
+Web browser version:
+- CSS
+- JavaScript/jQuery
 
 
-## Credits:
+## Credits
 - Pokemon images: [Pikachu](https://www.pokemon.com/us/pokedex/pikachu) | [Squirtle](https://www.pokemon.com/us/pokedex/squirtle) | [Charmander](https://www.pokemon.com/us/pokedex/charmander) | [Mystery Pokemon image](https://lolfilter.com/effect/1540755265106170/Guess%20the%20Pokemon!)
 - Game background image: [Bahnijit Barman](https://unsplash.com/@bahnijitb?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText) on [Unsplash](https://unsplash.com/s/photos/pokemon?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText)
-  
--- app.js
+++ app.js
@@ -1,198 +1,166 @@
 /////// CHOOSE YOUR OWN ADVENTURE GAME: POKEMON TRAINER! ///////////
-const prompt = require('prompt-sync')();
 
-/////// VARIABLES
-let pokemonHealth = 10;
-let pokemonFriendship = 0;
-let pokemonSkills = [];
+// console.log("LINKED");
+$(() => {
 
-/////// STARTER POKEMON ARRAY
-const pokemonOptions = [
-    // index 0:  0   -  1
+    /////// STARTER POKEMON ARRAY ///////
+    const pokemonOptions = [
+        // index 0:  0   -  1
         ["Pikachu", "electric"],
-    // index 1:  0   -  1
+        // index 1:  0   -  1
         ["Charmander", "fire"],
-    // index 2:  0   -  1
+        // index 2:  0   -  1
         ["Squirtle", "water"]
-];
-
-/////// POKEMON MOVES ARRAY
-const movesPikachu = ["Quick Attack", "Thunder Shock"];
-const movesCharmander = ["Scratch", "Ember"];
-const movesSquirtle =["Tail Whip", "Water Gun"];
-
-
-// 1. GAME STARTS. PLAYER ENTERS NAME
-console.log(`\nWelcome new Pokemon Trainer!!`)
-    const userName = prompt(`>> What name would you like to use? Type name here and press ENTER ... `);
-    console.log(`Nice to meet you, Trainer ${userName}!`);
-
-// 2. PLAYER CHOOSES A POKEMON - 3 CHOICES
-console.log(`\nTime to pick your starter Pokemon!`)
-    starterPokemon()
-
-    //////// FUNCTION TO CHOOSE 1 STARTER POKEMON
-    function starterPokemon(){
-        const starterPokemonChoice = prompt(`>> Choose one: [1] ${pokemonOptions[0][0]}, [2] ${pokemonOptions[1][0]}, or [3] ${pokemonOptions[2][0]} ... `);
-        // OPTION FOR PIKACHU, ELECTRIC
-        if(starterPokemonChoice === "1"){
-            currentPokemon = pokemonOptions[0][0];
-            console.log(`\nYou chose ${currentPokemon}!`);
-            console.log(`Pokedex data says... ${pokemonOptions[0][0]} is an ${pokemonOptions[0][1]} type Pokemon! Cute!`);
-        // OPTION FOR CHARMANDER, FIRE
-        }else if(starterPokemonChoice === "2"){
-            currentPokemon = pokemonOptions[1][0]
-            console.log(`\nYou chose ${currentPokemon}!`);
-            console.log(`Pokedex data says... ${pokemonOptions[1][0]} is a ${pokemonOptions[1][1]} type Pokemon! So cool!`);
-        // OPTION FOR SQUIRTLE, WATER
-        }else if(starterPokemonChoice === "3"){
-            currentPokemon = pokemonOptions[2][0]
-            console.log(`\nYou chose ${currentPokemon}!`);
-            console.log(`Pokedex data says... ${pokemonOptions[2][0]} is a ${pokemonOptions[2][1]} type Pokemon! Tubular!`);
-        }
+    ];
+    /////// POKEMON MOVES ARRAY ///////
+    const movesPikachu = ["Quick Attack", "Thunder Shock"];
+    const movesCharmander = ["Scratch", "Ember"];
+    const movesSquirtle = ["Tail Whip", "Water Gun"];
+    /////// PLAYER OBJECT /////// 
+    const playerInfo = {
+        name: '',
+        pokemon: [],
+        pokemonSkills: [],
+        pokemonFriendship: 0,
+        pokemonHealth: 10
     }
 
+    // Gameplay round
+    function gameRound() {
+        displayPokemonSection()
+        chooseStarterPokemon()
+        pokemonLearnMoves()
+        feedPokemon()
+    };
 
-// 3. PLAYER TEACHES POKEMON NEW SKILL - 2 CHOICES - ADDS ITEMS TO pokemonSkills ARRAY
-console.log(`\nLet's teach ${currentPokemon} a new skill!`)
-    pokemonLearnMoves()
+    // Initialize game
+    displayPokemonSection()
 
-    //////// FUNCTION FOR TEACHING POKEMON SKILLS
-    function pokemonLearnMoves(){
-        if(currentPokemon === pokemonOptions[0][0]){
-            const pokemonMoves = prompt(`>> Which skill would you like to learn? [1] ${movesPikachu[0]} or [2] ${movesPikachu[1]} ... `);
-            if(pokemonMoves === "1"){
-                pokemonSkills.push(movesPikachu[0]);
-                pokemonHealth -= 10;
-                pokemonFriendship += 5;
-                console.log(`\n${currentPokemon} learned ${movesPikachu[0]}! Nice!`);
-                console.log(`${currentPokemon} used up 10 health points but increased friendship by +5!`);
-            }else if(pokemonMoves === "2"){
-                pokemonSkills.push(movesPikachu[1]);
-                pokemonHealth -= 10;
-                pokemonFriendship += 5;
-                console.log(`\n${currentPokemon} learned ${movesPikachu[1]}! Awesome!`);
-                console.log(`${currentPokemon} used up 10 health points but increased friendship by +5!`);
-            }
-        }else if(currentPokemon === pokemonOptions[1][0]){
-            const pokemonMoves = prompt(`>> Which skill would you like to learn? [1] ${movesCharmander[0]} or [2] ${movesCharmander[1]} ... `);
-            if(pokemonMoves === "1"){
-                pokemonSkills.push(movesCharmander[0]);
-                pokemonHealth -= 10;
-                pokemonFriendship += 5;
-                console.log(`\n${currentPokemon} learned ${movesCharmander[0]}! Nice!`);
-                console.log(`${currentPokemon} used up 10 health points but increased friendship by +5!`);
-            }else if(pokemonMoves === "2"){
-                pokemonSkills.push(movesCharmander[1]);
-                pokemonHealth -= 10;
-                pokemonFriendship += 5;
-                console.log(`\n${currentPokemon} learned ${movesCharmander[1]}! Awesome!`);
-                console.log(`${currentPokemon} used up 10 health points but increased friendship by +5!`);
-            }
-        }else if(currentPokemon === pokemonOptions[2][0]){
-            const pokemonMoves = prompt(`>> Which skill would you like to learn? [1] ${movesSquirtle[0]} or [2] ${movesSquirtle[1]} ... `);
-            if(pokemonMoves === "1"){
-                pokemonSkills.push(movesSquirtle[0]);
-                pokemonHealth -= 10;
-                pokemonFriendship += 5;
-                console.log(`\n${currentPokemon} learned ${movesSquirtle[0]}! Nice!`);
-                console.log(`${currentPokemon} used up 10 health points but increased friendship by +5!`);
-            }else if(pokemonMoves === "2"){
-                pokemonSkills.push(movesSquirtle[1]);
-                pokemonHealth -= 10;
-                pokemonFriendship += 5;
-                console.log(`\n${currentPokemon} learned ${movesSquirtle[1]}! Awesome!`);
-                console.log(`${currentPokemon} used up 10 health points but increased friendship by +5!`);
-            }
-        }
-    }
+    // Game display shows pokemon section stats empty
+    function displayPokemonSection() {
+        $('.no-pokemon-img').css('display', 'block');
+        $('.skill.first').append(`<span>${playerInfo.pokemonSkills[0] || ''}`);
+        $('.skill.second').append(`<span>${playerInfo.pokemonSkills[1] || ''}`);
+        $('#health').text(`${playerInfo.pokemonHealth}`);
+        $('#friend').text(`${playerInfo.pokemonFriendship}`);
+    };
 
+    // 1. Player clicks start game button, to begin
+    $('#button-start-game').on('click', () => {
+        // console.log('click');
+        $('#player-name-prompt').css('display', 'block');
+        $('#player-terminal-form').css('display', 'flex');
+    });
 
-// 4. PLAYER CHECKS POKEMON STATS - 4 CHOICES (pokemonHealth, pokemonFriendship, pokemonSkills, or ALL)
-console.log(`\nWow! Now that ${currentPokemon} learned a new skill, let's check on their current stats...`)
-    checkPokemonStats()
+    // 2. Event Listener: Player enters name
+    $('#player-terminal-form').on('submit', respondToPlayerNameCreation);
 
-////// FUNCTION FOR CHECKING CURRENT POKEMON STATS
-    function checkPokemonStats(){
-        const checkStats = prompt(`>> Choose one: [S] skills, [F] friendship, [H] health, [A] all stats or [E] exit ... `).toUpperCase();
-        // OPTION FOR SKILLS
-        if(checkStats === "S"){
-            console.log(`\n${currentPokemon} has learned ${pokemonSkills} skill(s).`);
-        // OPTION FOR FRIENDSHIP
-        }else if(checkStats === "F"){
-            console.log(`\n${currentPokemon} has ${pokemonFriendship} friendship points.`);
-            if(pokemonFriendship < 0){
-                console.log(`\nYIKES... Are you sure you wanna be a Pokemon Trainer???`)
-            }
-        // OPTION FOR HEALTH
-        }else if(checkStats === "H"){
-            console.log(`\n${currentPokemon} has ${pokemonHealth} health points.`);
-            if(pokemonHealth <= 10){
-                console.log(`\nOh no... it looks like ${currentPokemon} is hungry.`)
-            }
-        // OPTION FOR ALL STATS
-        }else if(checkStats === "A"){
-            console.log(`\n${currentPokemon} has learned ${pokemonSkills} skill(s).`);
-            console.log(`${currentPokemon} has ${pokemonFriendship} friendship points.`);
-            console.log(`${currentPokemon} has ${pokemonHealth} health points.`);
-            if(pokemonFriendship < 0){
-                console.log(`\nYIKES... Are you sure you wanna be a Pokemon Trainer?`)
-            }
-            if(pokemonHealth <= 10){
-                console.log(`Oh no... it looks like ${currentPokemon} is hungry.`)
-            }
-        // OPTION FOR EXIT
-        }else if(checkStats === "E"){
-            console.log(`\nWow! Look how far you've come, Trainer ${userName}!`);
-    }
-}
+    // 3. Respond to player name form submit
+    function respondToPlayerNameCreation(event) {
+        event.preventDefault()
+        playerInfo.name = $('#player-input').val()
+        // console.log(playerInfo.name);
+        $('#player-input').val('');
+        alert(`Nice to meet you, Trainer ${playerInfo.name}!`);
+        $('#player-terminal-form').css('display', 'none');
+        $('#player-name-prompt').css('display', 'none');
+        chooseStarterPokemon()
+    };
 
+    // 4. Player chooses starter pokemon
+    function chooseStarterPokemon() {
+        $('#starter-pokemon-prompt').css('display', 'flex');
+        $('#starter-pokemon-options').css('display', 'flex');
+    };
 
-// 5. BONUS: PLAYER FEEDS POKEMON - 3 CHOICES
-console.log(`\nHey what's that over there?! You found some food nearby!`)
-    // FOOD WILL INCREASE/DECREASE pokemonHealth AND pokemonFriendship
-    feedPokemon()
+    // 5. Event Listeners: Player clicks starter pokemon
+    // Pikachu
+    $('#chose-pikachu').on('click', () => {
+        // remove mystery img
+        $('.no-pokemon-img').css('display', 'none');
+        // add img
+        $('#pikachu-img').css('display', 'block');
+        // add pokemon to player array
+        const newPokemon = {
+            name: pokemonOptions[0][0],
+            type: pokemonOptions[0][1]
+        };
+        playerInfo.pokemon.push(newPokemon);
+        console.log(newPokemon);
+        // hide prompt text and buttons
+        $('#starter-pokemon-options').css('display', 'none');
+        $('#starter-pokemon-prompt').css('display', 'none');
+        showPokedex()
+    });
+    // Charmander
+    $('#chose-char').on('click', () => {
+        $('.no-pokemon-img').css('display', 'none');
+        $('#charmander-img').css('display', 'block');
+        const newPokemon = {
+            name: pokemonOptions[1][0],
+            type: pokemonOptions[1][1]
+        };
+        playerInfo.pokemon.push(newPokemon);
+        $('#starter-pokemon-options').css('display', 'none');
+        $('#starter-pokemon-prompt').css('display', 'none');
+        showPokedex()
+    });
+    // Squirtle
+    $('#chose-squirtle').on('click', () => {
+        $('.no-pokemon-img').css('display', 'none');
+        $('#squirtle-img').css('display', 'block');
+        const newPokemon = {
+            name: pokemonOptions[2][0],
+            type: pokemonOptions[2][1]
+        };
+        playerInfo.pokemon.push(newPokemon);
+        $('#starter-pokemon-options').css('display', 'none');
+        $('#starter-pokemon-prompt').css('display', 'none');
+        showPokedex()
+    });
 
-    //////// FUNCTION FOR FEEDING POKEMON
-    function feedPokemon(){
-        const feedPokemonChoice = prompt(`>> Choose one: [A] apple slices, [B] berries, or [C] candy wrapper ... `).toUpperCase();
-        // OPTION FOR APPLE SLICES
-        if(feedPokemonChoice === "A"){
-            pokemonHealth += 10;
-            pokemonFriendship += 5;
-            console.log(`\nYum! Apple slices are a great snack.`);
-            console.log(`${currentPokemon}'s health increased by +10!`);
-            console.log(`Your friendship with ${currentPokemon} increased by +5!`);
-        // OPTION FOR BERRIES
-        }else if(feedPokemonChoice === "B"){
-            pokemonHealth += 50;
-            pokemonFriendship += 20;
-            console.log(`\nDelicious! Berries are a main staple for Pokemon diets.`);
-            console.log(`${currentPokemon}'s health increased by +50!`);
-            console.log(`AWW! ${currentPokemon} wants to be your BFF! Your friendship increased by +20 <3`);
-        // OPTION FOR CANDY WRAPPER
-        }else if(feedPokemonChoice === "C"){
-            pokemonHealth -= 75;
-            pokemonFriendship -= 10;
-            console.log(`\nWhat?! That's garbage! Are you trying to kill ${currentPokemon}????`);
-            console.log(`${currentPokemon}'s health DECREASED by -75!`);
-            console.log(`Yikes! I don't think that tasted good. Watch out, ${currentPokemon} might just eat you instead. Friendship went down by -10`);
-        }
-    }
+    // 6. Show pokedex of current pokemon
+    function showPokedex() {
+        $('#pokedex-info').css('display', 'block');
+        $('.current-pokemon').text(`${playerInfo.pokemon.name}`);
+        console.log(playerInfo.pokemon.name);
+        $('#current-type').text(`${playerInfo.pokemon.type}`);
+        console.log(playerInfo.pokemon.type);
+        pokemonLearnMoves()
+    };
 
+    // 7. Player chooses skill to teach pokemon
+    function pokemonLearnMoves() {
+        // remove previous text
 
-// 6. PLAYER CAN CHECK STATS AGAIN OR EXIT
-console.log(`\nWould you like to check another stat?`)
-    checkPokemonStats()
+        // show prompt
 
+        // show button options
 
-// GAME IS COMPLETE! 
-console.log(`\nTrainer ${userName}, you and ${currentPokemon} really worked hard today!\nTime to rest up until tomorrow!\nGoodbye!!!!`);
-    process.exit()
+        // pokemon health decreases
 
+        // pokemon friendship increases
 
+        feedPokemon()
+    };
 
-// MORE OPTIONS TO INCLUDE FOR LATER:
-// FEED POKEMON AGAIN
-// TEACH POKEMON ANOTHER SKILL
-// CONSOLIDATE CODE
+    // 8. Player chooses snack to give pokemon
+    function feedPokemon() {
+        // remove previous text
+
+        // show prompt
+
+        // show button options
+
+        // pokemon health updates
+
+        // pokemon friendship updates
+
+        // next function
+    };
+
+    // 9. option for window prompt if health is low?
+
+    // 10. option for window prompt to end game, or start new
+
+});
